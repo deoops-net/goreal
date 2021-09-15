5948528f733005f2406b8adc0920b31dcf07b0ee
yuyang <yuyang@cnuri.cn>
2021-09-15 16:37:55 +0800

    adjustment of Yuan

--- types/measures/currency.go
+++ types/measures/currency.go
@@ -1,13 +1,16 @@
 package measures
 
-import "strconv"
+import (
+	"fmt"
+)
 
 // Yuan 元
 type Yuan float64
 
 // MarshalJSON 按元为单位时,货币精度两位
 func (y Yuan) MarshalJSON() ([]byte, error) {
-	return []byte(strconv.FormatFloat(float64(y), 'f', 2, 64)), nil
+	return []byte(fmt.Sprintf("%f", y)), nil
+	// return []byte(strconv.FormatFloat(float64(y), 'f', 2, 64)), nil
 }
 
 // TODO 银行家算法
